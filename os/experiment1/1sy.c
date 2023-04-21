df95267fb7c943ad5895b774bcaaf814bb9c9770
赵孜硕 <jerryzhao2002@126.com>
2023-04-21 16:48:08 +0800

    operating system

---
+++ os/experiment1/1sy.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include <syspes.h>  
+#include <sys/wait.h>
+int main(){
+	pid_t Child1;
+	Child1=fork();
+	if(Child1<0){
+		printf("error\n");
+		exit(0);
+	}else if(Child1==0){
+		printf("Child1 process!\n");
+		printf("PID=%d  PPID=%d \n",getpid(),getppid()); 
+		exit(0);
+	}else{
+		pid_t Child2;
+		Child2=fork();
+		if(Child2<0){ 
+			printf("fork faild\n");
+			exit(0);
+		}else if(Child2==0){
+			printf("Child2 process!\n");
+			printf("PID=%d  PPID=%d \n",getpid(),getppid()); 
+			exit(0);
+		}else{
+			wait(0);
+			printf("Parent process!\n");
+			printf("PID=%d  PPID=%d \n",getpid(),getppid()); 
+			exit(0);
+		}
+	}
+	return 0;
+}
