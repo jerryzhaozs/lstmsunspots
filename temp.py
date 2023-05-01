d089b881a44825f55e6df1836700ee8e74e5f69c
赵孜硕 <jerryzhao2002@126.com>
2023-05-01 19:47:35 +0800

    temp

---
+++ temp.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torchvision import datasets, models, transforms
+
+model_ft = models.resnet18(pretrained=True)
+# num_ftrs = model_ft.fc.in_features
+# model_ft.fc = nn.Linear(num_ftrs, 2)
+# model_ft = model_ft.to(device)
+print(model_ft.parameters())
+
+# for param in model_ft.parameters():
+    # param.requires_grad=False
+    # print(param)
