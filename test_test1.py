95c720b0b2bbe308cd723072470e340ab34e1665
赵孜硕 <jerryzhao2002@126.com>
2023-06-23 17:22:06 +0800

    nc_csv_test

---
+++ test_test1.py
@@ -0,0 +1,52 @@
+import netCDF4 as nc
+import numpy as np
+import pandas as pd
+import csv
+import datetime
+import math
+def depose1(x):
+    nearest_number = math.floor(x) + 0.25 if (x % 1) <= 0.5 else math.floor(x) + 0.75
+    return nearest_number
+# 读取.nc4文件
+nc_file = nc.Dataset(r"H:\Py-nc-pshw-test\py_nc\PSHW\prob_cd_end_rm_maize_rain_long.nc4")
+csv_file=r'H:\Py-nc-pshw-test\py_nc\coord.txt'
+df = pd.read_csv(csv_file)
+# 读取coord字段中的纬度和经度信息
+nc_lat = nc_file.variables["lat"][:]
+nc_lon = nc_file.variables["lon"][:]
+nc_time=nc_file.variables["time"][:]
+value_values = nc_file.variables['var'][:]
+# print(df['lat'])
+# print(nc_lat)
+# df.to_csv("your_output.csv", index=False)
+start_date = datetime.datetime(year=1900, month=1, day=1)
+with open('output.csv', 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    header = ['lat', 'lon']
+    for time in nc_time:
+        date = start_date + datetime.timedelta(days=int(time))
+        header.append(date.strftime("%j"))
+    writer.writerow(header)
+
+    # now=df[["lon", "lat"]]
+    # print(now)
+    # now=df.iloc[1:]
+    for index, row in df.iterrows():
+        # print( latlon)
+        # print('!')
+        df_lat=row['lat']
+        df_lon=row['lon']
+        df_lat_use=depose1(df_lat)
+        df_lon_use=depose1(df_lon)
+        input_data=[df_lat,df_lon]
+        # for lat_idx, lat in enumerate(nc_lat):
+        #     if df_lat_use!=lat:
+        #         continue
+        #     for lon_idx, lon in enumerate(nc_lon):
+        #         if df_lon_use!=lon:
+        #             continue
+        latitude_index = np.abs(nc_lat - df_lat_use).argmin()
+        longitude_index = np.abs(nc_lon - df_lon_use).argmin()
+        for time_idx, time in enumerate(nc_time):
+            input_data.append(value_values[time_idx,latitude_index,longitude_index])
+        writer.writerow(input_data)
